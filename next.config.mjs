4f9fa883d1796473c3f4e416856a4a611373ec0a
ks-wook <ksu864@gmail.com>
2026-01-17 00:43:57 +0900

    배포환경 로그 미출력 처리

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    compiler: {
+    removeConsole: process.env.NODE_ENV === 'production'
+      ? { exclude: ['error'] } // console.error는 남김
+      : false,
+    },
 };
 
 export default nextConfig;
